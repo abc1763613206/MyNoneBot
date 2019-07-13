774bd8fca60e56ab3814910093418069ef3de1ee
abc1763613206 <abc1763613206@163.com>
2019-07-13 13:32:42 +0800

    fix:

--- plugins/txai_chat/chat.py
+++ plugins/txai_chat/chat.py
@@ -1,5 +1,5 @@
 # 参照 https://ai.qq.com/doc/sdk.shtml
-# from .apiutil import *
+from .apiutil import *
 import apiutil
 
 app_id = session.bot.config.TX_CHAT_APPID
@@ -12,7 +12,7 @@ async def call_txchat_api(session: CommandSession, text: str) -> Optional[str]:
     if not text:
         return None
 
-    ai_obj = apiutil.AiPlat(app_id, app_key)
+    ai_obj = AiPlat(app_id, app_key)
     rsp = ai_obj.getNlpTextChat(text,context_id(session.ctx, use_hash=True))
     if rsp['ret'] == 0:
         return json.dumps(rsp, encoding="UTF-8", ensure_ascii=False, sort_keys=False, indent=4)
