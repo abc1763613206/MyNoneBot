0a27513422c3ec7f216c074ca886a1a8869f27ad
abc1763613206 <abc1763613206@163.com>
2019-07-14 12:28:45 +0800

    fix: add retry

--- plugins/time_report.py
+++ plugins/time_report.py
@@ -12,10 +12,14 @@ async def _():
     bot = nonebot.get_bot()
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
     try:
-        
-        msg = f'Hi,现在{now.hour}点整啦！'
-        webresult = requests.get('https://api.ihcr.top/hitokoto/', timeout=1)
-        data = json.loads(webresult.text)
+        retry_count = 5
+        msg = f'Hi, 现在{now.hour}点整啦！'
+        while retry_count > 0:
+        try:
+            webresult = requests.get('https://api.ihcr.top/hitokoto/', timeout=3)
+            data = json.loads(webresult.text)
+        except Exception:
+            retry_count -= 1
         msg = msg + '\n--------------\n本小时的一言(Hitokoto):\n'+data['content']+'\n'+data['translation']+'\n—— '+data['origin']['singer']+' 《'+data['origin']['title']+'》\n'
         for gid in bot.config.MANGROUP:
             await bot.send_group_msg(group_id=gid,
