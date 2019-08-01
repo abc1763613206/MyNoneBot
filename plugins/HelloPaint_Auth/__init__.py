0836dbfdc1c38ed78e98627d4e34da7f0cab28e0
abc1763613206 <abc1763613206@163.com>
2019-08-01 23:46:35 +0800

    feat: add auth for paintboard

---
+++ plugins/HelloPaint_Auth/__init__.py
@@ -0,0 +1,18 @@
+import asyncio
+import json
+from nonebot import MessageSegment
+from nonebot import on_command, CommandSession
+
+def make_response(ok, result):
+    from json import JSONEncoder
+    return JSONEncoder().encode({"ok": ok, "result": result})
+
+bot = nonebot.get_bot()
+
+@bot.server_app.route("/paintboard/send_code", methods=["POST"])
+def paintboard_send_code():
+    token, target, content = request.form["token"], request.form["target"], request.form["content"]
+    if token != bot.config.TOKEN:
+        return make_response(False, {"message": "token错误"})
+    await bot.send_private_msg(user_id=target, message=content)
+    return make_response(True)
